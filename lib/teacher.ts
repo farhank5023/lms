1fd1173f7cb2eb921b8f4982df70542cb44b7073
farhank5023 <farhank5023@gmail.com>
2023-09-24 14:42:25 +0530

    update logo

--- lib/teacher.ts
+++ lib/teacher.ts
@@ -1,3 +1,8 @@
 export const isTeacher = (userId?: string | null) => {
-  return userId === process.env.NEXT_PUBLIC_TEACHER_ID;
+  return (
+    
+    userId === process.env.NEXT_PUBLIC_TEACHER_ID
+    //  || userId === process.env.NEXT_PUBLIC_TEACHER_ID2 
+    // use above code for multiple teachers
+    )
 }
