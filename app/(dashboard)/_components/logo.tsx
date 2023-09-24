263a2bfb04f0fae1d71292c9f84d08e4d0b6c015
farhank5023 <farhank5023@gmail.com>
2023-09-24 14:46:48 +0530

    update logo

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export const Logo = () => {
   return (
     <Image
-      height={60}
-      width={60}
+      height={90}
+      width={90}
       alt="logo"
       src="/logo-black.png"
     />
